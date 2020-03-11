79e74acc1fafe661b28d351b38ea94026801dd2e
Dylan MacKenzie <dmackenz@uoguelph.ca>
2020-03-11 15:10:19 -0400

    Initial commit

---
+++ test/_element.visual-diff.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const VisualDiff = require('@brightspace-ui/visual-diff');
+
+describe('<%= name %>', () => {
+
+	const visualDiff = new VisualDiff('<%= shortName %>', __dirname);
+
+	let browser, page;
+
+	before(async() => {
+		browser = await puppeteer.launch();
+		page = await browser.newPage();
+		await page.setViewport({width: 800, height: 800, deviceScaleFactor: 2});
+		await page.goto(`${visualDiff.getBaseUrl()}/test/<%= shortName %>.visual-diff.html`, {waitUntil: ['networkidle0', 'load']});
+		await page.bringToFront();
+	});
+
+	beforeEach(async() => {
+		await visualDiff.resetFocus(page);
+	});
+
+	after(() => browser.close());
+
+	it('passes visual-diff comparison', async function() {
+		const rect = await visualDiff.getRect(page, '#default');
+		await visualDiff.screenshotAndCompare(page, this.test.fullTitle(), { clip: rect });
+	});
+
+});
